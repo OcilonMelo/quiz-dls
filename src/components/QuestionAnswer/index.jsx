d5c1bdabc994e136fc521168fcd1a7a1cf77c1a6
Manoel Melo <ocilonmelo@gmail.com>
2023-03-15 19:49:41 -0300

    Feat: add initial version

---
+++ src/components/QuestionAnswer/index.jsx
@@ -0,0 +1,15 @@
+import S from './styles.module.scss'
+
+export function QuestionAnswer({ 
+    question,
+    answer,
+    handleAnswerQuestion
+}) {
+    return (
+        <button className={S.container} 
+        onClick={(event) => handleAnswerQuestion(event, question, answer)}
+        >
+            {answer}
+        </button>
+    )
+}
